02bcc4aca485fbe4b87a11b37ec2b61be74dc2ae
priyankaaallam <priyanka.a@techouts.com>
2022-03-14 15:29:45 +0530

    Priya

---
+++ javaVariables/Swapping2numbers.java
@@ -0,0 +1,16 @@
+package javaVariables;
+
+public class Swapping2numbers {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println("Before swapping:"+a+" "+b);
+		int c=a;
+		
+a=b;
+b=c;
+System.out.println("After swapping:"+a+" "+b);
+	}
+
+}
---
+++ javaVariables/TypeCasting.java
@@ -0,0 +1,13 @@
+package javaVariables;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		float f=10.2f;
+		int a=(int)f;
+		System.out.println(f);
+		System.out.println(a);
+		
+	}
+
+}
